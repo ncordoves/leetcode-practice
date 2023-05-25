254d4134c63003d4590553f68a241f36caaea5c4
ncordoves <nataliecordoves@gmail.com>
2023-05-25 14:32:32 -0400

    Adds sleep solution

--- sleep.js
+++ sleep.js
@@ -1,2 +1,5 @@
 // Given a positive integer millis, write an asyncronous function that sleeps for millis milliseconds. It can resolve any value.
 
+async function sleep(millis) {
+    return new Promise(delayresolve => setTimeout(delayresolve, millis));
+  }
