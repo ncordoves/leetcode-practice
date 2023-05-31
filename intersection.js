5a111ec81bcf57f9551b782250bdb18573af3730
ncordoves <nataliecordoves@gmail.com>
2023-05-31 16:37:11 -0400

    Adds intersection solution

--- intersection.js
+++ intersection.js
@@ -1,1 +1,12 @@
 // Construct a function intersection that compares input arrays and returns a new array with elements found in all of the inputs. BONUS - Use reduce!
+function intersection(array) {
+    return array.reduce((firstArr, currentArr) => {
+      const newArr = [];
+      firstArr.forEach(el => {
+        if (currentArr.includes(el)) {
+          newArr.push(el);
+        }
+      })
+      return newArr;
+    })
+  }
